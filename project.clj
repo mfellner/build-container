cb271d6c2a509ee16de787a2138ba86e45718b62
Maximilian Fellner <max.fellner@gmail.com>
2015-06-11 22:31:26 +0200

    Added sources

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject hellow "0.1.0-SNAPSHOT"
+  :description "Clojure in a container."
+  :url "https://github.com/mfellner/build-container"
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [ring/ring-core "1.4.0-RC1"]
+                 [ring/ring-devel "1.4.0-RC1"]
+                 [ring/ring-jetty-adapter "1.4.0-RC1"]]
+  :plugins [[lein-ring  "0.9.5"]]
+  :main ^:skip-aot hello.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+  :ring {:handler hello.core/handler})
